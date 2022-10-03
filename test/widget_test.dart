92b9047ce29b105281c7bf29441da9dae46161ed
KolasikOmetov <42410594+KolasikOmetov@users.noreply.github.com>
2022-10-03 20:25:31 +0400

    feat: add test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,10 +7,31 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:sspr_test/counter.dart';
 import 'package:sspr_test/main.dart';
 
 void main() {
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
+
+    test('value should be incremented', () {
+      final counter = Counter();
+
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
+  });
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
